36866ac5e4490af3bd3fb15373f28232823e602d
Mannie <manshook@gmail.com>
2018-02-02 12:50:09 -0500

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;  
+
+	public KeyCode rightKey = KeyCode.RightArrow;
+	public KeyCode leftKey = KeyCode.LeftArrow;
+
+	public float bottomOfScreen = -5f;
+
+	public string sceneToSwitchToOnDeath = "GameOverScene";
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		if (Input.GetKey(rightKey)) {
+			transform.Translate(moveSpeed, 0, 0);
+		}
+		if (Input.GetKey(leftKey)) {
+			transform.Translate(-moveSpeed, 0, 0);
+		}
+
+		if (transform.position.y < bottomOfScreen) {
+			SceneManager.LoadScene(sceneToSwitchToOnDeath);
+		}
+	}
+}
---
+++ Assets/ballControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballControl : MonoBehaviour {
+	public GameObject colBox;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnMouseDown (){
+		print ("touch");
+		gameObject.GetComponent<Mover> ().enabled = false;
+		gameObject.GetComponent<Rigidbody> ().isKinematic = false;
+
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject == colBox) {
+			Destroy (gameObject);
+		}
+	}
+}
